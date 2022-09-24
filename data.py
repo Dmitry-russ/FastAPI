7f43f15cb0edd065b8687fd58a53c48a0aef3139
Dmitry-russ <dapetuhov@yandex.ru>
2022-09-24 22:58:24 +0500

    настроил redoc

--- data.py
+++ data.py
@@ -69,8 +69,9 @@ def data_load():
     return data_base, classies_keys
 
 
+#  для примеров запросов и ответов в redoc
 post_examples = {
-    "odd": {
+    "new": {
         "summary": "пример",
         "value": {
             "name": "Гора",
@@ -83,7 +84,7 @@ post_examples = {
 }
 
 patch_examples = {
-    "odd": {
+    "change": {
         "summary": "пример",
         "value": {
             "description": "Замей горыныч",
@@ -91,3 +92,205 @@ patch_examples = {
         }
     },
 }
+
+get_responses = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "get": {
+                        "summary": "get_all",
+                        "value": {
+                            "dragon": {
+                                0: {
+                                    "name": "Горыныч",
+                                    "weight": 240,
+                                    "description": "описание",
+                                    "magic": "огонь",
+                                    "wingspan": 143,
+                                    "max_altitude": 2500,
+                                }
+                            },
+                            "chimera": {
+                                0: {
+                                    "name": "Матильда",
+                                    "weight": 100,
+                                    "description": "описание",
+                                    "magic": "огонь",
+                                    "max_speed": 135,
+                                }
+                            },
+                            "basilisk": {
+                                0: {
+                                    "name": "Зубр",
+                                    "weight": 112,
+                                    "description": "описание",
+                                    "magic": "яд",
+                                    "strenght": 145,
+                                }
+                            },
+                        }
+                    }
+                },
+            }
+        }
+    }
+}
+
+get_class_response = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "get_class_response": {
+                        "summary": "get_class_response",
+                        "value": {
+                            0: {
+                                "name": "Зубр",
+                                "weight": 112,
+                                "description": "описание",
+                                "magic": "яд",
+                                "strenght": 145,
+                            },
+                            1: {
+                                "name": "Матильда",
+                                "weight": 100,
+                                "description": "описание",
+                                "magic": "огонь",
+                                "max_speed": 135,
+                            }
+
+                        }
+                    }}}}},
+    400: {
+        "description": "Bad Request",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "get_class_response": {
+                        "summary": "get_class_response",
+                        "value": {"error": "Class does not exist in data_base"}
+                    }
+                }}}}
+}
+
+get_id_response = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "get_id_response": {
+                        "summary": "get_id_response",
+                        "value": {
+                            "name": "Зубр",
+                            "weight": 112,
+                            "description": "описание",
+                            "magic": "яд",
+                            "strenght": 145,
+                        }
+                    }
+                }}}},
+    400: {
+        "description": "Bad Request",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "get_id_response": {
+                        "summary": "get_id_response",
+                        "value":
+                            {"error": "id not found"}
+
+                    }
+                }}}}
+
+}
+
+del_response = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "del_response": {
+                        "summary": "del_response",
+                        "value": "Deleted: type {someone}, id {id}"
+                    }
+                }}}}}
+
+post_response = {
+    201: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "post_response": {
+                        "summary": "post_response",
+                        "value": {"created": {0: {
+                            "name": "Гора",
+                            "weight": 500,
+                            "description": "Замей горыныч",
+                            "magic": "Огонь",
+                            "wingspan": 100
+                        }}}
+                    }
+                }}}},
+    400: {
+        "description": "Bad Request",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "post_response": {
+                        "summary": "post_response",
+                        "value": {"error": "Class does not exist"}
+                    }
+                }}}},
+    406: {
+        "description": "Not Acceptable",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "post_response": {
+                        "summary": "post_response",
+                        "value": {"error": "Not such attr. in this class. Check your data."}
+                    }
+                }}}},
+
+}
+
+patch_response = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "patch_response": {
+                        "summary": "patch_response",
+                        "value": {"changed": {0: {
+                            "name": "Гора",
+                            "weight": 500,
+                            "description": "Замей горыныч",
+                            "magic": "Огонь",
+                            "wingspan": 100
+                        }}}
+                    }
+                }}}},
+    400: {
+        "description": "Bad Request",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "class problem": {
+                        "summary": "class problem",
+                        "value": {"error": "Class does not exist in data_base."}
+                    },
+                    "id problem": {
+                        "summary": "id problem",
+                        "value": {"error": "id not found."}
+                    }
+                },
+            }
+        }
+    }}
--- main.py
+++ main.py
@@ -1,61 +1,75 @@
 import uvicorn
 from fastapi import FastAPI, Body
+from fastapi.responses import JSONResponse
 
-from data import data_load, post_examples, patch_examples
+from data import (data_load,
+                  post_examples,
+                  patch_examples, get_responses,
+                  get_class_response, get_id_response,
+                  del_response, post_response, patch_response)
 
 data_base, classies_keys = data_load()
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", responses=get_responses)
 async def get_all():
     return data_base
 
 
-@app.get("/{someone}/")
+@app.get("/{someone}/", status_code=200, responses=get_class_response)
 async def get_someone(someone: str):
     if someone not in data_base:
-        return {"error": "Class does not exist in data_base"}
+        return JSONResponse(
+            content={"error": "Class does not exist in data_base"},
+            status_code=400)
     return data_base[someone]
 
 
-@app.get("/{someone}/{id}")
+@app.get("/{someone}/{id}", responses=get_id_response)
 async def get_someone_id(someone: str, id: int):
     someone_data: dict = data_base[someone]
     if id not in someone_data.keys():
-        return {"error": "id not found"}
+        return JSONResponse(content={"error": "id not found"}, status_code=400)
     return someone_data[id]
 
 
-@app.post("/{someone}/")
+@app.post("/{someone}/", status_code=201, responses=post_response)
 async def new_someone(someone: str, item: dict = Body(examples=post_examples)):
     if someone not in classies_keys:
-        return {"error": "Class does not exist"}
+        return JSONResponse(
+            content={"error": "Class does not exist"}, status_code=400)
     try:
         someone_new = classies_keys[someone](**item)
     except TypeError:
-        return {"error": "Not such attr. Check your data."}
+        return JSONResponse(
+            content={
+                "error": "Not such attr. in this class. Check your data."},
+            status_code=406)
     count: int = list(data_base[someone])[-1] + 1 if data_base[someone] else 0
     data_base[someone][count] = someone_new
-    return someone_new
+    return {"created": {count: someone_new}}
 
 
-@app.patch("/{someone}/{id}")
+@app.patch("/{someone}/{id}", responses=patch_response)
 async def change_someone(
         someone: str, id: int, item: dict = Body(examples=patch_examples)):
     if someone not in data_base:
-        return {"error": "Class does not exist in data_base."}
+        return JSONResponse(
+            content={"error": "Class does not exist in data_base."},
+            status_code=400)
     someone_data: dict = data_base[someone]
     if id not in someone_data:
-        return {"error": "id not found."}
+        return JSONResponse(
+            content={"error": "id not found."}, status_code=400)
     for key in item:
         if key in dir(someone_data[id]):
             setattr(someone_data[id], key, item[key])
-    return someone_data[id]
+    return {"changed": {id: someone_data[id]}}
 
 
-@app.delete("/{someone}/{id}")
+@app.delete("/{someone}/{id}", responses=del_response)
 async def del_someone(someone: str, id: int):
     if someone not in data_base:
         return {"error": "Class does not exist in data_base"}
